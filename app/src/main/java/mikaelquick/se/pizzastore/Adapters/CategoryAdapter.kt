f91f57e8bc13e62aa314c3b6aef6daf4f294c8be
Mikael Quick <mikaelquick@live.se>
2019-01-21 00:51:45 +0100

    can increase and decrease the number

--- app/src/main/java/mikaelquick/se/pizzastore/Adapters/CategoryAdapter.kt
+++ app/src/main/java/mikaelquick/se/pizzastore/Adapters/CategoryAdapter.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import mikaelquick.se.pizzastore.API.Resturant
 import mikaelquick.se.pizzastore.Models.MenuObjects
@@ -25,6 +26,10 @@ class CategoryAdapter(val context: Context): RecyclerView.Adapter<CategoryAdapte
         val item = items[postion]
         holder.name.text = item.name
         holder.price.text = returnPrice(item)
+
+        if(holder.amount.tag == null){
+            holder.amount.tag = 0
+        }
 
         if(item.category == cat){
             holder.headContainer.visibility = View.GONE
@@ -50,6 +55,32 @@ class CategoryAdapter(val context: Context): RecyclerView.Adapter<CategoryAdapte
             holder.topping.visibility = View.GONE
         }
 
+        holder.addButton.setOnClickListener({
+            changeAmount(holder,increase = true)
+        })
+
+        holder.deleteButton.setOnClickListener({
+            changeAmount(holder,increase = false)
+        })
+    }
+
+   private fun changeAmount(holder: CategoryViewHolder,increase:Boolean = true){
+        try {
+            var amount = holder.amount.tag as Int
+            if(increase){
+                amount += 1
+            }
+            else{
+                if(amount > 0){
+                    amount -= 1
+                }
+            }
+            holder.amount.text = amount.toString()
+            holder.amount.tag = amount
+        }
+        catch (e:Exception){
+            Log.e("CAT",e.message)
+        }
     }
 
     private var items = mutableListOf<MenuObjects>()
@@ -79,10 +110,13 @@ class CategoryAdapter(val context: Context): RecyclerView.Adapter<CategoryAdapte
     }
 
     class CategoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var amount: TextView = itemView.findViewById(R.id.cat_amount)
         var headContainer: ConstraintLayout = itemView.findViewById(R.id.cat_headContainer)
         var headTitle: TextView = itemView.findViewById(R.id.cat_headTitle)
         var name: TextView = itemView.findViewById(R.id.cat_name)
         var price: TextView = itemView.findViewById(R.id.cat_price)
         var topping: TextView = itemView.findViewById(R.id.cat_topping)
+        var addButton: ImageView = itemView.findViewById(R.id.cat_addButton)
+        var deleteButton: ImageView = itemView.findViewById(R.id.cat_delteButton)
     }
 }
